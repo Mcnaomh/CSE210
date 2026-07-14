9227d1d43ff27d0cdfdaefdd12825433e720b2da
Mcnaomh <wjensm@gmail.com>
2026-07-14 11:08:00 -0600

    week 3 learning activity

---
+++ week03/Fractions/Fraction.cs
@@ -0,0 +1,52 @@
+using System.Dynamic;
+
+public class Fraction
+{
+    private int _top;
+    private int _bottom;
+
+    public Fraction()
+    {
+        _top = 1;
+        _bottom = 1;
+    }
+
+    public Fraction(int wholeNumber)
+    {
+        _top = wholeNumber;
+        _bottom = 1;
+    }
+
+    public Fraction(int top, int bottom)
+    {
+        _top = top;
+        _bottom = bottom;
+    }
+
+    public void setTop(int top)
+    {
+        _top = top;
+    }
+    public void setBottom(int bottom)
+    {
+        _bottom = bottom;
+    }
+    public int getTop()
+    {
+        return _top;
+    }
+    public int getBottom()
+    {
+        return _bottom;
+    }
+
+    public string GetFractionString()
+    {
+        string text = $"{_top}/{_bottom}";
+        return text;
+    }
+    public double GetDecimalValue()
+    {
+        return (double)_top / (double)_bottom;
+    }
+}
