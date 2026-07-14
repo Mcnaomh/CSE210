6002ea79d55dbc323440c3632ce7f95c1cdad93f
Mcnaomh <wjensm@gmail.com>
2026-07-14 16:57:14 -0600

    Changes

--- week03/Fractions/Fraction.cs
+++ week03/Fractions/Fraction.cs
@@ -4,19 +4,16 @@ public class Fraction
 {
     private int _top;
     private int _bottom;
-
     public Fraction()
     {
         _top = 1;
         _bottom = 1;
     }
-
     public Fraction(int wholeNumber)
     {
         _top = wholeNumber;
         _bottom = 1;
     }
-
     public Fraction(int top, int bottom)
     {
         _top = top;
