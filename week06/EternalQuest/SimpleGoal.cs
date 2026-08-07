0c23a6ca305077522f9c456a2a7720eb47c5f063
Mcnaomh <wjensm@gmail.com>
2026-08-07 10:20:14 -0600

    Final code for the week 6

---
+++ week06/EternalQuest/SimpleGoal.cs
@@ -0,0 +1,32 @@
+public class SimpleGoal : Goal
+{
+    private bool _isComplete;
+
+    public SimpleGoal(string name, string description, int points)
+        : base(name, description, points)
+    {
+        _isComplete = false;
+    }
+
+    public SimpleGoal(string name, string description, int points, bool complete)
+        : base(name, description, points)
+    {
+        _isComplete = complete;
+    }
+
+    public override int RecordEvent()
+    {
+        _isComplete = true;
+        return _points;
+    }
+
+    public override bool IsComplete()
+    {
+        return _isComplete;
+    }
+
+    public override string GetStringRepresentation()
+    {
+        return $"SimpleGoal:{_name},{_description},{_points},{_isComplete}";
+    }
+}
